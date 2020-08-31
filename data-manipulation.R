ec5b7f543d712d2df483e3d83a4fb0e80d395c1c
Nick Griffiths <ngriffiths21@gmail.com>
2020-08-31 11:37:17 -0400

    df recode

--- data-manipulation.R
+++ data-manipulation.R
@@ -1,3 +1,9 @@
 # recode from a lookup table
+data = c("a", "b", "b")
 lookup = list(a = "new a", b = "new b")
 do.call(dplyr::recode, c(list(data), lookup))
+
+# recode from a lookup dataframe
+data = c("a", "b", "b")
+lookup = data.frame(name = c("a", "b"), val = c("new a", "new b"))
+do.call(recode, c(list(data), setNames(lookup$val, lookup$name)))
